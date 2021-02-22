fb242e0819784abac8227bbbedf22f070de7b47b
wangsiyuan06 <wangsiyuan06@baidu.com>
2021-02-22 17:07:08 +0800

    fix

--- cpp/include/deploy/common/blob.h
+++ cpp/include/deploy/common/blob.h
@@ -17,6 +17,7 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <algorithm>
 
 namespace Deploy {
 
@@ -53,9 +54,9 @@ class ShapeInfo{
   std::vector<std::string> transform_order;
 
   // transform index
-  int GetIndex(const string &name) {
-    vector<string>::iterator it =
-      find(transform_order.begin(), transform_order.end(), name);
+  int GetIndex(const std::string &name) {
+    std::vector<string>::iterator it =
+      std::find(transform_order.begin(), transform_order.end(), name);
     if (it != transform_order.end()) {
       return (it - transform_order.begin());
     } else {
@@ -65,27 +66,24 @@ class ShapeInfo{
   }
   // im_info = {h, w, scale_w}
   std::vector<float> GetImInfo() {
-    int transforms_num = (*shape_traces)[i].shape.size() - 1;
-    float new_w =
-          static_cast<float>((*shape_traces)[i].shape[transforms_num][0]);
-    float origin_w = static_cast<float>((*shape_traces)[i].shape[0][0]);
+    int transforms_num = shape.size() - 1;
+    float new_w = static_cast<float>(shape[transforms_num][0]);
+    float origin_w = static_cast<float>(shape[0][0]);
     float scale_w = new_w / origin_w;
     std::vector<float> im_info =
-          {static_cast<float>((*shape_traces)[i].shape[transforms_num][1]),
-          static_cast<float>((*shape_traces)[i].shape[transforms_num][0]),
+          {static_cast<float>(shape[transforms_num][1]),
+          static_cast<float>(shape[transforms_num][0]),
           scale_w};
     return im_info;
   }
 
   // scale = {scale_w, scale_h}
   std::vector<float> GetScale() {
-    int transforms_num = (*shape_traces)[i].shape.size() - 1;
-    float new_w =
-          static_cast<float>((*shape_traces)[i].shape[transforms_num][0]);
-    float new_h =
-          static_cast<float>((*shape_traces)[i].shape[transforms_num][1]);
-    float origin_w = static_cast<float>((*shape_traces)[i].shape[0][0]);
-    float origin_h = static_cast<float>((*shape_traces)[i].shape[0][1]);
+    int transforms_num = shape.size() - 1;
+    float new_w = static_cast<float>(shape[transforms_num][0]);
+    float new_h = static_cast<float>(shape[transforms_num][1]);
+    float origin_w = static_cast<float>(shape[0][0]);
+    float origin_h = static_cast<float>(shape[0][1]);
     float scale_w = new_w / origin_w;
     float scale_h = new_h / origin_h;
     std::vector<float> scale = {scale_w, scale_h};
